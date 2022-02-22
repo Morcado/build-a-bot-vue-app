e790af98ea8cf7a8cbd9acdf8b8c0178e71c3b3f
oagonzalez <oagonzalez@elpotosi.com.mx>
2022-02-22 16:32:38 -0600

    agregar carrito de compras v-for

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
+  <div class="content">
+    <button class="add-cart" @click="addToCart()">
+      Add to cart
+    </button>
     <div class="top-row">
       <div class="top part">
         <div class="robot-name">
           {{selectedRobot.head.title}}
+          <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
         </div>
         <img :src="selectedRobot.head.src" title="head"/>
         <button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
@@ -34,6 +38,23 @@
         <button class="next-selector">&#9658;</button>
       </div>
     </div>
+    <div>
+      <h2>Cart</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -54,6 +75,7 @@ export default {
   data() {
     return {
       avaliableParts,
+      cart: [],
       selectedHeadIndex: 0,
       selectedLeftArmIndex: 0,
       selectedTorsoIndex: 0,
@@ -73,6 +95,13 @@ export default {
     },
   },
   methods: {
+    addToCart() {
+      const robot = this.selectedRobot;
+      const cost = robot.head.cost + robot.leftArm.cost +
+        robot.torso.cost + robot.rightArm.cost + robot.base.cost;
+      // no copiar la instancia Object.assign({}, robot, { cost })
+      this.cart.push({ ...robot, cost });
+    },
     selectNextHead() {
       this.selectedHeadIndex =
         getNextValidIndex(this.selectedHeadIndex, avaliableParts.heads.length);
@@ -180,5 +209,29 @@ export default {
   top: -25px;
   text-align: center;
   width: 100%;
+}
+.sale {
+  color: red;
+}
+.content {
+  position: relative;
+}
+
+.add-to-cart {
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-size: 16px;
+}
+
+td, th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+
+.cost {
+  text-align: right;
 }
 </style>
