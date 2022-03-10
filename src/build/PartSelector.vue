e1e168ef7b027f892bf3e96afc686ff838821699
oagonzalez <oagonzalez@elpotosi.com.mx>
2022-03-10 11:38:46 -0600

    corregir error

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -10,7 +10,8 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin:position.top.right class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin="{ bottom: '10px', right: '5px' }"
+      class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
@@ -28,7 +29,7 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
-  diretives: { pin: pinDirective },
+  directives: { pin: pinDirective },
   props: {
     parts: {
       type: Array,
--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-param-reassign */
 export default {
   beforeMount(el, binding) {
-    if (binding.arg !== 'position') return;
-
-    Object.keys(binding.modifiers).forEach((key) => {
-      el.style[key] = '5px';
+    Object.keys(binding.value).forEach((position) => {
+      el.style[position] = binding.value[position];
     });
     el.style.position = 'absolute';
   },
